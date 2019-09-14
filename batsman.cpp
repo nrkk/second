101a233e2428a7ca62bcb659cbd27377eb33ebf5
nrkk <nrkk.1.2.3@gmail.com>
2019-09-14 12:08:00 +0530

    constr

---
+++ batsman.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+static int count=-1;
+class batsman
+{
+private:
+int runs;
+public:
+batsman()
+{
+count++;
+}
+void score();
+void ipl();
+void icc();
+void wc();
+void odi();
+};
+
+
+
+void batsman::score()
+{
+void ipl();
+void icc();
+void wc();
+void odi();
+
+}
+void batsman::ipl()
+{
+batsman a[2]; 
+}
+
+void batsman::icc()
+{
+batsman b[3]; 
+}
+
+void batsman::wc()
+{
+batsman c[5]; 
+}
+
+void batsman::odi()
+{
+batsman d[11]; 
+
+}
+
+
+int main()
+{
+batsman b;
+b.score();
+cout<<"total objects created:"<<count;
+return 0;
+}
+
